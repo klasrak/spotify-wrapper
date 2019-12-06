b4dde43f6a73b46c934499db9f5d236d08932f6b
Danilo Augusto <dasf_cm@terra.com.br>
2019-12-06 11:04:01 -0300

    fix dependecies

---
+++ examples/album.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/first */
+global.fetch = require('node-fetch')
+
+import { searchAlbums } from '../src/main'
+
+const albums = searchAlbums('Incubus')
+
+albums.then(data => data.albums.items.map(item => console.log(item.name)))
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch')
+
 export const search = (query, type) => {
   fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
     .then(data => data.json())
